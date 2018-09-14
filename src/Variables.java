fd119aa548eb3b4c454a6b5362d59e31e8548c52
rohitharidas123 <42977203+rohitharidas123@users.noreply.github.com>
2018-09-13 20:50:52 -0400

    Add files via upload

---
+++ src/Variables.java
@@ -0,0 +1,33 @@
+/** 
+ * Variable Homework 9/13
+ * @author Rohit
+ *
+ */
+public class Variables
+{
+	public static void main (String[] args)
+	{
+		String name = "Rohit";
+		System.out.print("Hi, my name is ");
+		System.out.println(name);
+		
+		int length = 4;
+		int width = 5;
+		int area = length*width;
+			System.out.print("The area is ");
+			System.out.println(area);
+			
+		int radius = 3;
+		double pi = 3.14;
+		double circlearea = pi*radius*radius;
+			System.out.print("The area is ");
+			System.out.println(circlearea);
+			
+		int diameter = 6;
+		double circum = pi*diameter;
+			System.out.print("The circumference is ");
+			System.out.println(circum);
+		
+		
+	}
+}
